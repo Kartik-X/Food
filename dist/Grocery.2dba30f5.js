4fa6282814bf3d909176cc78779c1d8dfb503de0
Mohini kulkarni <meta619012@gmail.com>
2024-08-27 12:57:45 +0530

    check

---
+++ dist/Grocery.2dba30f5.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequirec9a9;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0"),i=()=>(0,t.jsx)("h1",{children:"Grocery online store.We have lots of child components in this web-pege"})});
+//# sourceMappingURL=Grocery.2dba30f5.js.map
